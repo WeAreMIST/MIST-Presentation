59a2ecccb1e8401ca6c9a3d205c70bf2caefb9b7
Anuraag <baishya.anuraag1920@gmail.com>
2016-06-03 09:30:59 +0530

    First Commit

---
+++ mist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+progress.sh "Checking for System Vulnaribilities...."
+progress.sh "Checking for Network Loopholes...."
+progress.sh "Setting up Exploits...."
+progress.sh "Extracting Payloads...."
+progress.sh "Completing Initialisation...."
+echo 'MIST Shell Initialised'
+sleep 0.25
+rr.sh /home/anuraag/Documents/Projects/Shell/mist_pres/text_art
+printf 'Enter Username: '
+read username
+printf 'Enter Password: '
+read -s password
+if [ "$username" = "darkoperator" ] && [ "$password" = "password" ]; then 
+    echo ' '
+    echo 'Login Succesful'
+    sleep 0.25
+    mist_next.sh  
+fi;
---
+++ mist_next.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+rr.sh /home/anuraag/Documents/Projects/Shell/mist_pres/page1
+read -s next
+clear
+rr.sh /home/anuraag/Documents/Projects/Shell/mist_pres/page2
+read -s next
+clear
+rr.sh /home/anuraag/Documents/Projects/Shell/mist_pres/page3
+read -s next
