cd3b7315ae41513e054fd4f0fcb9e22226c7f87d
Anuraag <baishya.anuraag1920@gmail.com>
2016-09-30 11:00:10 +0530

    Changes for first year GBM

---
+++ mist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+sleep 0.25
+cat /home/anuraag/Documents/Projects/Scripts/mist_pres/text_art | nms
+mist_next.sh
---
+++ mist_next.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+cat  /home/anuraag/Documents/Projects/Scripts/mist_pres/page1 | nms
+read -s next
+clear
+cat /home/anuraag/Documents/Projects/Scripts/mist_pres/page2 | nms
+read -s next
+clear
+cat /home/anuraag/Documents/Projects/Scripts/mist_pres/page4 | nms
+read -s next
+clear
+cat /home/anuraag/Documents/Projects/Scripts/mist_pres/page5 | nms
+read -s next
